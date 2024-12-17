d131a534938398d8dc4cad1af76d917d303b01b1
cappydino <mincrftrulz@gmail.com>
2024-12-17 16:35:08 -0500

    Oh. That's how

--- day_17/pt2.py
+++ day_17/pt2.py
@@ -1,60 +1,47 @@
 with open('./day_17/input.txt', 'r') as file:
     inp = file.read().split("\n\n")
 
-import re
+instructions = list(map(int, inp[1][9:].split(",")))
 
-A, B, C = list(map(int, re.findall(r"\d+", inp[0])))
+instructions = list(reversed(instructions))
 
-instructions = list(map(int, re.findall(r"\d+", inp[1])))
+def run(a):
+    res = []
+    A = a
+    B = 0
+    C = 0
+    while A > 0:
+        B = A % 8
+        B = B ^ 5
+        C = A // (2**B)
+        B = B ^ 6
+        A = A // 8
+        B = B ^ C
+        res.append(B % 8)
+    return res
 
+def to_num(A):
+    res = 0
+    for i in range(len(A)):
+        res += A[i] * (8**(len(A) - i - 1))
+    return res
 
-def run(A, B, C, instructions):
-    ptr = 0
-    output_num = 0
-
-    def combo(operand):
-        if operand == 4:
-            return A
-        if operand == 5:
-            return B
-        if operand == 6:
-            return C
-        return operand
-
-    while ptr < len(instructions):
-        opcode = instructions[ptr]
-        operand = instructions[ptr + 1]
-
-        match opcode:
-            case 0:
-                A = A // (2**combo(operand))
-            case 1:
-                B = B ^ operand
-            case 2:
-                B = combo(operand) % 8
-            case 3:
-                if A != 0:
-                    ptr = combo(operand) - 2
-            case 4:
-                B = B ^ C
-            case 5:
-                if instructions[output_num] != combo(operand) % 8:
-                    return False
-                else:
-                    output_num += 1
-            case 6:
-                B = A // (2**combo(operand))
-            case 7:
-                C = A // (2**combo(operand))
-
-        ptr += 2
-    return output_num == len(instructions)
-
-i = 1087000000
+A = [1]
+i = 0
 while True:
-    i += 1
-    if i % 1000000 == 0:
-        print(i)
-    if run(i, B, C, instructions):
-        print(i)
-        exit()
+    if A[i] == 7:
+        A.pop()
+        i -= 1
+        A[i] += 1
+        continue
+
+    r = list(reversed(run(to_num(A))))
+
+    if r[i] == instructions[i]:
+        if len(r) == len(instructions):
+            print(to_num(A))
+            exit()
+        A.append(0)
+        i += 1
+    else:
+        A[i] += 1
